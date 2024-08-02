03569039952f4b351c60fa31deb19d5b296cd275
Культяпов Константин <pozys@mail.ru>
2024-08-03 01:03:37 +0300

    fix bug in setting create view

--- resources/views/settings/create.blade.php
+++ resources/views/settings/create.blade.php
@@ -1,5 +1,5 @@
 <x-setting.form method='POST' :action="route('settings.store')" :title="__('setting.create.title')" :$documentTypes
-    :$documentFormats :$schemaElements :$type>
+    :$documentFormats :$schemaElements :$type :setting=null>
     <x-slot:submit>
         <x-forms.submit :text="__('setting.create.submit')" />
         </x-slot>

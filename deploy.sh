c08c71b2b115bbb87b83426d959ca3fafc439a76
Культяпов Константин <pozys@mail.ru>
2024-08-04 05:16:06 +0300

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ sudo sed -i -- "s|%SERVER_NAME%|$1|g" $2/external/nginx/conf.d/app.conf
 sudo sed -i -- "s|%APP_URL%|$3|g" .env
 sudo sed -i -- "s|%DB_PASSWORD%|$4|g" .env
 
+make install-prod
 make db-migrate
 make db-seed
-make install-prod
 make build-frontend
 make compose-restart

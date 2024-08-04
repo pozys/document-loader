c5d2800fff3814f2ad6dd417c86d6b89b8fd30ea
Культяпов Константин <pozys@mail.ru>
2024-08-04 05:11:44 +0300

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 set -e
 make env-prepare
 sudo sed -i -- "s|%SERVER_NAME%|$1|g" $2/external/nginx/conf.d/app.conf
-sudo sed -i -- "s|%APP_URL%|$2|g" .env
-sudo sed -i -- "s|%DB_PASSWORD%|$3|g" .env
+sudo sed -i -- "s|%APP_URL%|$3|g" .env
+sudo sed -i -- "s|%DB_PASSWORD%|$4|g" .env
 
 make db-migrate
 make db-seed

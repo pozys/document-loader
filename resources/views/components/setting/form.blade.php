9b883a69e36b0a1a4b9f64767244c349c7c412b2
Культяпов Константин <pozys@mail.ru>
2024-08-04 18:24:27 +0300

    update form.blade.php

--- resources/views/components/setting/form.blade.php
+++ resources/views/components/setting/form.blade.php
@@ -5,6 +5,36 @@
     {{ html()->hidden('user_id', $setting?->user_id ?? auth()->user()->id) }}
 
     <div class="flex flex-col">
+        <div>
+            {{ html()->label( __('setting.name'), 'setting')->class('font-black') }}
+        </div>
+        <div class="mt-2">
+            {{ html()->text('name')->class('rounded border-gray-300 w-1/3')->required() }}
+            @error('name')
+            <x-forms.validation :$message />
+            @enderror
+        </div>
+        <div class="mt-2">
+            {{ html()->label( __('setting.document_type'), 'document_type')->class('font-black') }}
+        </div>
+        <div>
+            {{ html()->select('document_type', $documentTypes)
+            ->class('rounded border-gray-300 w-1/3')->required() }}
+            @error('document_type')
+            <x-forms.validation :$message />
+            @enderror
+        </div>
+        <div class="mt-2">
+            {{ html()->label( __('setting.document_format'), 'document_format')->class('font-black') }}
+        </div>
+        <div>
+            {{ html()->select('document_format', $documentFormats)
+            ->class('rounded border-gray-300 w-1/3')->required() }}
+            @error('document_format')
+            <x-forms.validation :$message />
+            @enderror
+        </div>
+
         <div class="w-full flex items-center">
             <div class="ml-auto">
                 <a href="{{ route('settings.create') }}"
